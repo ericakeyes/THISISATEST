b770c0dfb32d300b5a5273b9e2e8d4350087bb29
ericakeyes <ekeyes@celerium.com>
2023-08-31 08:47:50 -0400

    init commit

---
+++ Tools/src/celerium/com/tools/MakeMariaFiles.java
@@ -0,0 +1,53 @@
+package celerium.com.tools;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MakeMariaFiles {
+
+	public static void main(String[] args) {
+		final String baseDirectory = "C:\\Users\\EricaKeyes\\Documents\\GitHub\\cdnservices\\src\\main\\resources\\queries";
+			
+		File srcDir = new File(baseDirectory);
+
+		for (File dir : srcDir.listFiles())
+        {
+           if ( dir.isDirectory()) {
+        	   
+        	   for (File file : dir.listFiles())
+               {
+                  if (file.isFile())
+                  {
+                	  System.out.println("file is "+file.getName());
+                	  System.out.println("file is "+file.getPath().replace("-oracle","-mariadb"));
+                	  String newFileName = file.getPath().replace("-oracle","-mariadb");
+                	  FileOutputStream out = null;
+                	  try {
+						  out = new FileOutputStream(newFileName);
+						  Path path = Paths.get( file.getPath());
+						  Files.copy( path, out);
+					} catch (IOException e) {
+						System.out.println("IO ERROR : "+e.toString());
+						e.printStackTrace();
+					}finally
+                	 {
+						 try
+				            {
+				                out.close();
+				            }
+				            catch (IOException e1)
+				            {
+				                e1.printStackTrace();
+				            }
+                	  }
+                  }
+               }     
+           }
+        }
+	}
+
+}
